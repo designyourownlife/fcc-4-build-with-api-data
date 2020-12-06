8fa381d7200aff600f5face1467e2054f5eedeef
m/s <m@umbr-ch.de>
2020-12-06 21:44:46 +0100

    Test commit

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,9 +1,19 @@
 const axios = require("axios");
+const format = require('date-fns/format');
+const deLocale = require('date-fns/locale/de');  
 require('dotenv').config();
+
+const formattedDate = publishDate => {
+  const stPattysDay = new Date(publishDate);
+  return format(stPattysDay, 'dd MMMM yyyy', { locale: deLocale });
+}
+
+exports.formattedDate = formattedDate;
+
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
