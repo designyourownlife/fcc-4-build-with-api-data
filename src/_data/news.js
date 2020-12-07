79fa9d19bfb99216fcfaf71267cbd72c3014d931
m/s <m@umbr-ch.de>
2020-12-07 17:39:45 +0100

    added date formatting using date-fns

--- src/_data/news.js
+++ src/_data/news.js
@@ -6,14 +6,15 @@ require('dotenv').config();
 
 const formattedDate = publishDate => {
   const stPattysDay = new Date(publishDate);
-  return format(stPattysDay, 'dd MMMM yyyy', { locale: deLocale });
+  return format(stPattysDay, 'dd.MM.yyyy, hh:mm', { locale: deLocale });
 }
-
-exports.formattedDate = formattedDate;
+console.log(formattedDate('2020-12-07T10:26:00Z'));
+//exports.formattedDate = formattedDate;
 
 async function getNews(country) {
   try {
     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    
     return {
       "country": country, 
       "articles": response.data.articles
@@ -27,7 +28,13 @@ module.exports = async function() {
   
   let newsPromises = countries.map(getNews);
   return Promise.all(newsPromises).then( newsObjects => {
-    console.log('newsObjects: ', newsObjects);
+    newsObjects.forEach(obj => {
+       obj.articles.forEach((article, index) => {
+         article.publishedAt = formattedDate(article.publishedAt);
+         //console.log(article.publishedAt)
+       });
+    });
+    //console.log('newsObjects: ', newsObjects);
     return [].concat.apply([], newsObjects)
   })
 
