00e3a08a04f2cc2104db3cf0bb0ba988fbadc2ea
m/s <m@umbr-ch.de>
2020-12-07 11:33:47 +0100

    localized news

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const countries = require("./countries.json")
 const format = require('date-fns/format');
 const deLocale = require('date-fns/locale/de');  
 require('dotenv').config();
@@ -10,13 +11,24 @@ const formattedDate = publishDate => {
 
 exports.formattedDate = formattedDate;
 
-
-module.exports = async function() {
+async function getNews(country) {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-    return response.data;
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    return {
+      "country": country, 
+      "articles": response.data.articles
+    }
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function() {
+  
+  let newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then( newsObjects => {
+    console.log('newsObjects: ', newsObjects);
+    return [].concat.apply([], newsObjects)
+  })
 
 };
